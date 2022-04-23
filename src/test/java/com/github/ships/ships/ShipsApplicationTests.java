1d325ec2a92ab2990e5f7d0919e088e03c7a6515
Damian Ciepiel <dciepiela@yahoo.com>
2022-04-23 12:14:12 +0200

    init Application

---
+++ src/test/java/com/github/ships/ships/ShipsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.ships.ships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipsApplicationTests {
+
+    /**
+     * Default test to check if application boots
+     */
+    @Test
+    void contextLoads() {
+    }
+
+}
