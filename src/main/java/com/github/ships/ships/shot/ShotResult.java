d817854542224e4f987a781eafffc814f690afef
maciej blok <maciej_blok@epam.comm>
2022-04-28 08:47:06 +0200

    feat: shot

--- src/main/java/com/github/ships/ships/shot/ShotResult.java
+++ src/main/java/com/github/ships/ships/shot/ShotResult.java
@@ -9,10 +9,26 @@ public class ShotResult {
 
     private ShotLegality shotLegality;
 
+    /*
+        Defined if shotLegality == LEGAL
+        Defined by Board if hit cell was water.
+        Defined by Fleet if hit cell was not water.
+     */
     private StatusOfLegalShot statusOfLegalShot;
 
+    /*
+        Defined by Board if shotLegality is legal.
+     */
     private int cellIDofLegalShot;
+
+    /*
+        Defined by Fleet in case of sunk ship.
+     */
     private List<Integer> shipSunk;
+
+    /*
+        Defined by Fleet in case of sunk ship.
+     */
     private List<Integer> adjWaterOfShipSunk;
 
 }
