08d817df2e631573894ce6e6644f0be239d70881
Kamil Wilk <eventide666@protonmail.com>
2022-04-28 22:20:30 +0200

    feat: frontend

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,8 +1,21 @@
-let stompClient = null;
+var stompClient = null;
+var notificationCount = 0;
 
 $(document).ready(function() {
     console.log("Index page is ready");
     connect();
+
+    $("#send").click(function() {
+        sendMessage();
+    });
+
+    $("#send-private").click(function() {
+        sendPrivateMessage();
+    });
+
+    $("#notifications").click(function() {
+        resetNotificationCount();
+    });
 });
 
 function connect() {
@@ -10,8 +23,52 @@ function connect() {
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/user/events', function (message) {
-            console.log(JSON.parse(message.body).content);
+        updateNotificationDisplay();
+        stompClient.subscribe('/events/messages', function (message) {
+            console.log(message)
+            showMessage(JSON.parse(message.body).content);
         });
+
+        stompClient.subscribe('/user/events', function (message) {
+            showMessage(JSON.parse(message.body).content);
+        });
+
+        // stompClient.subscribe('/topic/global-notifications', function (message) {
+        //     notificationCount = notificationCount + 1;
+        //     updateNotificationDisplay();
+        // });
+        //
+        // stompClient.subscribe('/user/topic/private-notifications', function (message) {
+        //     notificationCount = notificationCount + 1;
+        //     updateNotificationDisplay();
+        // });
     });
+}
+
+function showMessage(message) {
+    $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+function sendMessage() {
+    console.log("sending message");
+    stompClient.send("/ws/message", {}, JSON.stringify({'content': $("#message").val()}));
+}
+
+function sendPrivateMessage() {
+    console.log("sending private message");
+    stompClient.send("/ws/private-message", {}, JSON.stringify({'content': $("#private-message").val()}));
+}
+
+function updateNotificationDisplay() {
+    if (notificationCount == 0) {
+        $('#notifications').hide();
+    } else {
+        $('#notifications').show();
+        $('#notifications').text(notificationCount);
+    }
+}
+
+function resetNotificationCount() {
+    notificationCount = 0;
+    updateNotificationDisplay();
 }
