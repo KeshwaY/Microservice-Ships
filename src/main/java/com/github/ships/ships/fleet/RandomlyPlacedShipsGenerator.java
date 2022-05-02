ead61c39cc5b3632a059919fe36399652e367d62
Maciej Blok <maciekblok@gmail.com>
2022-05-02 14:16:37 +0200

    refactor: extracts PossibleShipPositionsGenerator

--- src/main/java/com/github/ships/ships/fleet/RandomlyPlacedShipsGenerator.java
+++ src/main/java/com/github/ships/ships/fleet/RandomlyPlacedShipsGenerator.java
@@ -1,9 +1,12 @@
 package com.github.ships.ships.fleet;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/**
+ * Basing on ship sizes and board dimensions, provides randomly generated ship positions.
+ *
+ * @author Maciej Blok
+ */
 class RandomlyPlacedShipsGenerator {
 
     private final int playerId;
@@ -11,7 +14,6 @@ class RandomlyPlacedShipsGenerator {
     private final int boardHeight;
     private List<Integer> shipSizes;
     private Set<Integer> occupiedCells;
-
 
     public RandomlyPlacedShipsGenerator(int playerId, int boardWidth, int boardHeight) {
         this.playerId = playerId;
@@ -22,6 +24,11 @@ class RandomlyPlacedShipsGenerator {
         shipSizes.addAll(List.of(1, 1, 1, 1, 2, 2, 2, 3, 3, 4));
     }
 
+    /**
+     * Provides randomly generated ship positions.
+     *
+     * @return {@link java.util.List}
+     */
     public List<Ship> produceShips() {
         List<Ship> ships = new ArrayList<>();
         Random random = new Random();
@@ -34,47 +41,9 @@ class RandomlyPlacedShipsGenerator {
     }
 
     private Ship generateShip(int shipSize) {
-        List<List<Integer>> allShipPositions = new ArrayList<>();
-        addAllHorizontalPositions(allShipPositions, shipSize);
-        if(shipSize > 1) addAllVerticalPositions(allShipPositions, shipSize);
+        List<List<Integer>> allLegalShipPositions = new PossibleShipPositionsGenerator(boardWidth, boardHeight).
+                generateShipPositions(shipSize, occupiedCells);
 //TODO add to set masts and adjacent cells
         return null;
-    }
-
-    private void addAllHorizontalPositions(List<List<Integer>> allShipPositions, int shipSize) {
-        List<List<Integer>> allHorizontalPositions = new ArrayList<>();
-        Stream.iterate(1, i -> i + 1).limit(boardWidth * boardHeight - shipSize + 1).
-                forEach(i -> addHorizontalShipPosition(i, shipSize, allHorizontalPositions));
-        allHorizontalPositions.stream().filter(p -> isShipInOneBoardLine(p) && !isAnyPositionOccupied(p)).
-                forEach(p -> allShipPositions.add(p));
-    }
-
-    private void addHorizontalShipPosition(Integer cellId, int shipSize, List<List<Integer>> allHorizontalPositions) {
-        allHorizontalPositions.add(Stream.iterate(cellId, i -> i + 1).limit(shipSize).collect(Collectors.toList()));
-    }
-
-    private boolean isShipInOneBoardLine(List<Integer> shipCells) {
-        return shipCells.get(shipCells.size() - 1) % boardWidth == 0 ||
-                !shipCells.stream().anyMatch(i -> i % boardWidth == 0);
-    }
-
-    private void addAllVerticalPositions(List<List<Integer>> allShipPositions, int shipSize) {
-        List<List<Integer>> allVerticalPositions = new ArrayList<>();
-        Stream.iterate(1, i -> i + 1).limit(boardWidth * boardHeight - (shipSize - 1) * boardWidth).
-                forEach(i -> addVerticalShipPosition(i, shipSize, allVerticalPositions));
-        allVerticalPositions.stream().filter(p -> !isAnyPositionOccupied(p)).forEach(p -> allShipPositions.add(p));
-    }
-
-    private void addVerticalShipPosition(Integer cellId, int shipSize, List<List<Integer>> allVerticalPositions) {
-        allVerticalPositions.add(Stream.iterate(cellId, i -> i + boardWidth).limit(shipSize).
-                collect(Collectors.toList()));
-    }
-
-    private boolean isAnyPositionOccupied(List<Integer> masts) {
-        return masts.stream().anyMatch(m -> isPositionOccupied(m));
-    }
-
-    private boolean isPositionOccupied(Integer mast) {
-        return occupiedCells.stream().anyMatch(m -> m.equals(mast));
     }
 }
