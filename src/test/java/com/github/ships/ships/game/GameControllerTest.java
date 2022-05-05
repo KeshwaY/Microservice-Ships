3cec0ff64f4111d56539ab89f17805ae4e5c9677
Anna Ovcharenko <annaowczarenko@gmail.com>
2022-05-05 14:08:55 +0200

    feat: add board test

---
+++ src/test/java/com/github/ships/ships/game/GameControllerTest.java
@@ -0,0 +1,46 @@
+package com.github.ships.ships.game;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.ships.ships.board.Board;
+import com.github.ships.ships.board.BoardGetDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testng.annotations.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.testng.Assert.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GameControllerTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @WithMockUser(username = "test@2email.com", password = "test")
+    public void shouldCreateProperGame() throws Exception {
+        BoardGetDto boardGetDto = new BoardGetDto(10,10);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(boardGetDto);
+        mvc.perform(post("/api/v1/games")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+}
