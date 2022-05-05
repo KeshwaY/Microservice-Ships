9d8571c849c85f6abe5aa71516c8161e0ddbb38e
Damian Ciepiela <dciepiela@yahoo.com>
2022-05-05 19:32:05 +0200

    fix: test still needs connection to the database

--- src/test/java/com/github/ships/ships/game/GameControllerTest.java
+++ src/test/java/com/github/ships/ships/game/GameControllerTest.java
@@ -3,44 +3,62 @@ package com.github.ships.ships.game;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.github.ships.ships.board.Board;
-import com.github.ships.ships.board.BoardGetDto;
+import com.github.ships.ships.board.BoardPostDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.testng.Assert.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@ContextConfiguration
+@WebAppConfiguration
 public class GameControllerTest extends AbstractTestNGSpringContextTests {
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mvc;
 
+    @BeforeMethod
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
-    @WithMockUser(username = "test@2email.com", password = "test")
+    @WithUserDetails()
     public void shouldCreateProperGame() throws Exception {
-        BoardGetDto boardGetDto = new BoardGetDto(10,10);
+        GamePostDto gamePostDto = new GamePostDto();
+        BoardPostDto boardGetDto = new BoardPostDto(10,10);
+        gamePostDto.setBoard(boardGetDto);
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        String requestJson = ow.writeValueAsString(boardGetDto);
+        String requestJson = ow.writeValueAsString(gamePostDto);
+        System.out.println(requestJson);
         mvc.perform(post("/api/v1/games")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson)
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(user("test@email.com").password("test").authorities(new SimpleGrantedAuthority("BASIC_USER")))
+                )
                 .andDo(print())
-                .andExpect(status().isOk());
-
+                .andExpect(status().isCreated());
     }
 }
